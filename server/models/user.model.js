ebe46db3cf1440e539b14a81a9955976410a84d8
Hemanth-5 <22z225@psgtech.ac.in>
2024-05-12 10:49:15 +0530

    changes in backend

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
@@ -16,9 +16,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    avatar:{
+    avatar: {
       type: String,
-      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
     },
     gender: {
       type: String,
@@ -27,16 +28,16 @@ const userSchema = new mongoose.Schema(
     address: {
       type: String,
     },
-    conatct: {
+    contact: {
       type: String,
     },
     age: {
       type: Number,
-    }
+    },
   },
   { timestamps: true }
 );
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
