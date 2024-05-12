7ceab0b973704ae232f51da9d03fe5e725ffb50c
Hemanth-5 <22z225@psgtech.ac.in>
2024-05-12 20:02:31 +0530

    modified files

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,50 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import MainPage from "./Components/MainPage";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Patient from "./pages/Patient";
+import Hospital from "./pages/Hospital";
+import Donar from "./pages/Donar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const mainRoutes = [
+  // {
+  //   path: "/",
+  //   component: MainPage,
+  // },
+  {
+    path: "/login/user",
+    component: Login,
+  },
+  {
+    path: "/signup/user",
+    component: SignUp,
+  },
+  {
+    path: "/pages/Patient",
+    component: Patient,
+  },
+  {
+    path: "/pages/Hospital",
+    component: Hospital,
+  },
+  {
+    path: "/pages/Donar",
+    component: Donar,
+  },
+];
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      {mainRoutes.map((route, index) => (
+        <Route key={index} path={route.path} element={<route.component />} />
+      ))}
+      <Route path="*" element={<Navigate to="/login/user" />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
