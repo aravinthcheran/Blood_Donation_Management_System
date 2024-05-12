cb1254aac97f683a4c24a5b825a9214edccd5ec7
Hemanth-5 <22z225@psgtech.ac.in>
2024-05-12 11:26:27 +0530

    removed unnecessary files

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import Login from "./pages/Login"; // Corrected import
 import SignUp from "./pages/SignUp";
-import LoginDonor from "./pages/LoginDonor"; // Updated import
-import LoginHospital from "./pages/LoginHospital"; // Assuming you have a LoginHospital component
-import SignUpDonar from "./pages/SignUpDonar";
-import SignUpHospital from "./pages/SignUpHospital";
-import SignUpPatient from "./pages/SignUpPatient";
 import Patient from "./pages/Patient";
 import Hospital from "./pages/Hospital";
-import LoginPatient from "./pages/LoginPatient";
 import Donar from "./pages/Donar";
 
 const App = () => {
@@ -20,16 +14,8 @@ const App = () => {
         <Route path="/" element={<MainPage />} />
         <Route path="/login/user" element={<Login />} />
         <Route path="/signup/user" element={<SignUp />} />
-        <Route path="/pages/LoginDonar" element={<LoginDonor />} />
-        {/* Updated path */}
-        <Route path="/pages/LoginHospital" element={<LoginHospital />} />
-        {/* Assuming you have a LoginHospital component */}
-        <Route path="/pages/SignUpDonar" element={<SignUpDonar />} />
-        <Route path="/pages/SignUpHospital" element={<SignUpHospital />} />
-        <Route path="/pages/SignUpPatient" element={<SignUpPatient />} />
         <Route path="/pages/Patient" element={<Patient />} />
         <Route path="/pages/Hospital" element={<Hospital />} />
-        <Route path="/pages/LoginPatient" element={<LoginPatient />} />
         <Route path="/pages/Donar" element={<Donar />} />
       </Routes>
     </BrowserRouter>
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded w-75">
+        <h2 className="text-center">Login</h2>
+        <form className="row g-3">
+          <div className="col-md-6">
+            <label htmlFor="Email" className="form-label">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              name="Email"
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="col-md-6">
+            <label htmlFor="Password" className="form-label">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="Password"
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="col-md-12 text-center">
+            <button type="submit" className="btn btn-primary w-50 rounded-0 ">
+              <Link to="/pages/Donar" className="text-decoration-none">
+                Login
+              </Link>
+            </button>
+          </div>
+          <div className="col-md-12 text-center">
+            <p>
+              Don't have an account?{" "}
+              <Link to="/signup/user" className="text-decoration-none">
+                Sign Up
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
